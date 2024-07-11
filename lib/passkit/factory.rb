15e2fbdaab59ae7b0f73e0c0efb1483aa4519ba9
Maximiliano Pombo <16279542+maxipombo@users.noreply.github.com>
2024-07-11 10:06:58 -0600

    Check if exist

--- lib/passkit/factory.rb
+++ lib/passkit/factory.rb
@@ -3,7 +3,22 @@ module Passkit
     class << self
       # generator is an optional ActiveRecord object, the application data for the pass
       def create_pass(pass_class, generator = nil)
-        pass = Passkit::Pass.create!(klass: pass_class, generator: generator)
+        pass = if generator
+                Passkit::Pass.find_or_create_by!(
+                  klass: pass_class,
+                  generator_type: generator.class.name,
+                  generator_id: generator.id
+                )
+              else
+                Passkit::Pass.find_or_create_by!(klass: pass_class)
+              end
+
+        # Actualiza los datos del pase si es necesario
+        pass.update!(
+          serial_number: pass.serial_number || SecureRandom.uuid,
+          authentication_token: pass.authentication_token || SecureRandom.hex
+        )
+
         Passkit::Generator.new(pass).generate_and_sign
       end
     end
