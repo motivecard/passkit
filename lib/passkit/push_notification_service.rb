221638b518fd5e40f989c5069bc2ad942f44d38c
Maximiliano Pombo <16279542+maxipombo@users.noreply.github.com>
2024-07-13 10:23:03 -0600

    Minor fix devises

--- lib/passkit/push_notification_service.rb
+++ lib/passkit/push_notification_service.rb
@@ -10,7 +10,8 @@ module Passkit
 
     class << self
       def notify_pass_update(pass)
-        pass.devices.each do |device|
+        devices = Passkit::Device.where(passkit_pass_id: pass.id)
+        devices.each do |device|
           send_push_notification(device.push_token, pass.pass_type_identifier)
         end
       end
